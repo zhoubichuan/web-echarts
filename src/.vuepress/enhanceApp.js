fe4250102f06a22e37a4982f3c5c463233f0cac3
周必川 <zhoubichuan@icloud.com>
2023-05-23 22:36:15 +0800

    update

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -1,14 +1,9 @@
-// 通过该文件，把需要用的组件进行全局的注册，因为在markdown中的文件不能使用import引入组件，必须要提前注册好全局的组件
-import TableRender from '../senior/4.component/TableRender.vue';
-import xminder from './xmind.vue';
 import config from '@/config'
 import ui from '@/ui'
 
 export default ({
     Vue
 }) => {
-    Vue.component('TableRender', TableRender);
-    Vue.component('xminder', xminder);
     Vue.use(ui)
     Vue.use(config)
 }
