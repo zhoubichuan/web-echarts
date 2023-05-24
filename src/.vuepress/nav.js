7615d2db005b1a7be159781796163f8d2e25550b
周必川 <zhoubichuan@icloud.com>
2023-05-24 23:03:44 +0800

    update

--- src/.vuepress/nav.js
+++ src/.vuepress/nav.js
@@ -1,104 +1,88 @@
-const nav = [{
-    text: '基础知识',
+const nav = [
+  {
+    text: "基础知识",
     items: [
-        {
-            text: '一.折线图',
-            link: '/base/engine/1.index'
-        },
-        {
-            text: '二.柱状图',
-            link: '/base/project/1.index',
-        },
-        {
-            text: '三.饼图',
-            link: '/base/vue2.x/1.index'
-        },
-        {
-            text: '四.散点图',
-            link: '/base/vue3.x/1.index'
-        }
-    ]
-},
-{
-    text: '高级知识',
+      {
+        text: "一.常见部分",
+        link: "/base/engine/1.index",
+      },
+      {
+        text: "二.少见部分",
+        link: "/base/project/1.index",
+      },
+      {
+        text: "三.3D部分",
+        link: "/base/vue2.x/1.index",
+      },
+      {
+        text: "四.GL部分",
+        link: "/base/vue3.x/1.index",
+      },
+    ],
+  },
+  {
+    text: "canvas实现",
     items: [
-        {
-            text: '一.地理坐标/地图',
-            link: '/senior/1.micro/1.index'
-        },
+      {
+        text: "一.折线图",
+        link: "/senior/1.micro/1.index",
+      },
 
-        {
-            text: '二.漏斗',
-            link: '/senior/2.typescript/1.index'
-        },
-        {
-            text: '三.构建部署',
-            link: '/senior/3.deploy/1.index'
-        },
-        {
-            text: '四.组件开发',
-            link: '/senior/4.component/1.index'
-        },
-    ]
-},
-{
-    text: 'Vue 2.x源码',
-    items: [
-        {
-            text: '一.Vue/cli2.x',
-            link: '/source/vue-cli2.x/1.index'
-        },
-        {
-            text: '二.Vue 2.x',
-            link: '/source/vue2.x/1.index'
-        },
-        {
-            text: '三.Vue-Router3.x',
-            link: '/source/vue-router3.x/1.index'
-        },
-        {
-            text: '四.Vuex3.x',
-            link: '/source/vuex3.x/1.index'
-        }
-    ]
-},
-{
-    text: 'Vue 3.x源码',
-    items: [
-        {
-            text: '一.Vue/cli3.x',
-            link: '/source/vue-cli3.x/1.index'
-        },
-        {
-            text: '二.Vue 3.x',
-            link: '/source/vue3.x/1.index'
-        },
-        {
-            text: '三.Vue-Router4.x',
-            link: '/source/vue-router4.x/1.index'
-        },
-        {
-            text: '四.Vuex4.x',
-            link: '/source/vuex4.x/1.index'
-        }
-    ]
-},
-// {
-//     text: 'Vue项目',
+      {
+        text: "二.柱状图",
+        link: "/senior/2.typescript/1.index",
+      },
+      {
+        text: "三.饼图",
+        link: "/senior/3.deploy/1.index",
+      },
+      {
+        text: "四.关系图",
+        link: "/senior/4.component/1.index",
+      },
+    ],
+  },
+//   {
+//     text: "Vue 2.x源码",
 //     items: [
-//         {
-//             text: '一.点餐系统',
-//             link: 'https://github.com/zhoubichuan/order-vue.git'
-//         },
-//         {
-//             text: '二.ElementUI 组件',
-//             link: 'https://github.com/zhoubichuan/Web-ElementUI.git'
-//         }
-//     ]
-// },
-{
-    text: 'Vue资料',
-    link: '/read',
-}
-]
-module.exports = nav
+//       {
+//         text: "一.Vue/cli2.x",
+//         link: "/source/vue-cli2.x/1.index",
+//       },
+//       {
+//         text: "二.Vue 2.x",
+//         link: "/source/vue2.x/1.index",
+//       },
+//       {
+//         text: "三.Vue-Router3.x",
+//         link: "/source/vue-router3.x/1.index",
+//       },
+//       {
+//         text: "四.Vuex3.x",
+//         link: "/source/vuex3.x/1.index",
+//       },
+//     ],
+//   },
+//   {
+//     text: "Vue 3.x源码",
+//     items: [
+//       {
+//         text: "一.Vue/cli3.x",
+//         link: "/source/vue-cli3.x/1.index",
+//       },
+//       {
+//         text: "二.Vue 3.x",
+//         link: "/source/vue3.x/1.index",
+//       },
+//       {
+//         text: "三.Vue-Router4.x",
+//         link: "/source/vue-router4.x/1.index",
+//       },
+//       {
+//         text: "四.Vuex4.x",
+//         link: "/source/vuex4.x/1.index",
+//       },
+//     ],
+//   },
+];
+module.exports = nav;
