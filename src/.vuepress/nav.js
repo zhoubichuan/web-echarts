0c3e83183e359db2c10ae89f92fc4b87741d6a16
zhoubichuan <zhoubichuan@icloud.com>
2024-03-31 23:02:45 +0800

    更新文档

--- src/.vuepress/nav.js
+++ src/.vuepress/nav.js
@@ -20,49 +20,28 @@ const nav = [
       },
     ],
   },
-  {
-    text: "canvas实现",
-    items: [
-      {
-        text: "一.折线图",
-        link: "/senior/1.micro/1.index",
-      },
+  // {
+  //   text: "canvas实现",
+  //   items: [
+  //     {
+  //       text: "一.折线图",
+  //       link: "/senior/1.micro/1.index",
+  //     },
 
-      {
-        text: "二.柱状图",
-        link: "/senior/2.typescript/1.index",
-      },
-      {
-        text: "三.饼图",
-        link: "/senior/3.deploy/1.index",
-      },
-      {
-        text: "四.关系图",
-        link: "/senior/4.component/1.index",
-      },
-    ],
-  },
-    {
-      text: "zrender",
-      items: [
-        {
-          text: "一.zrender案例",
-          link: "/source/vue-cli2.x/1.index",
-        },
-        {
-          text: "二.zrender",
-          link: "/source/vue2.x/1.index",
-        },
-        {
-          text: "三.Vue-Router3.x",
-          link: "/source/vue-router3.x/1.index",
-        },
-        {
-          text: "四.Vuex3.x",
-          link: "/source/vuex3.x/1.index",
-        },
-      ],
-    },
+  //     {
+  //       text: "二.柱状图",
+  //       link: "/senior/2.typescript/1.index",
+  //     },
+  //     {
+  //       text: "三.饼图",
+  //       link: "/senior/3.deploy/1.index",
+  //     },
+  //     {
+  //       text: "四.关系图",
+  //       link: "/senior/4.component/1.index",
+  //     },
+  //   ],
+  // },
     {
       text: "Echarts源码",
       items: [
